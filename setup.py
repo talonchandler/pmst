206d065957df49d838a1459a5f4aa63225fc76d5
Talon Chandler <talonchandler@talonchandler.com>
2016-10-07 10:15:12 -0500

    Added package to PyPi

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pmst',
       version='0.1',
       description='Polarized microscope simulation tool.',
-      url='',
+      url='https://github.com/talonchandler/pmst',
       author='Talon Chandler',
       author_email='talonchandler@uchicago.edu',
       license='MIT',
-      package=['pmst'],
+      packages=['pmst'],
       zip_safe=True)
