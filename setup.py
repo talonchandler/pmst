02cf986b2efcda7c136150eddefca8e56af9e355
Talon Chandler <talonchandler@talonchandler.com>
2016-10-07 09:49:40 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pmst',
+      version='0.1',
+      description='Polarized microscope simulation tool.',
+      url='',
+      author='Talon Chandler',
+      author_email='talonchandler@uchicago.edu',
+      license='MIT',
+      package=['pmst'],
+      zip_safe=True)
