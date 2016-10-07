a982d9b4e6626915d3fbd11ee3bb007007f7ccbb
Talon Chandler <talonchandler@talonchandler.com>
2016-10-07 10:47:50 -0500

    Descriptions of package

---
+++ README.rst
@@ -0,0 +1,9 @@
+``pmst``
+----
+
+``pmst`` - Polarized Microscope Simulation Tool
+
+``pmst`` is a A 3D ray tracing simulation tool that predicts the output of polarized
+microscopes. The user can specify the geometry and properties of the light
+source, optics, sample, and detector via a Python interface, and ``pmst``
+will predict the output image.
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='pmst',
       version='0.1',
       description='Polarized microscope simulation tool.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Scientific/Engineering :: Medical Science Apps.',
+      ],
       url='https://github.com/talonchandler/pmst',
       author='Talon Chandler',
       author_email='talonchandler@uchicago.edu',
       license='MIT',
       packages=['pmst'],
-      zip_safe=True)
+      install_requires=[
+          'matplotlib',
+          'numpy',
+      ],
+      zip_safe=False)
