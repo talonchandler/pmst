7534b105e2bb3e763ebf6828aa1f99c25cadf52f
Talon Chandler <talonchandler@talonchandler.com>
2016-10-17 17:35:03 -0500

    Fixed detector off-by-one bug.

--- pmst/detector.py
+++ pmst/detector.py
@@ -3,12 +3,24 @@ import numpy as np
 
 
 class Detector(Plane):
-    """Plane detector with square pixels"""
+    """Square plane detector with square pixels.
+
+    Arguments:
+    p1 = center point coodinates
+    p2 = x edge coordinates
+    p3 = y edge coordinates
+    xnpix = number of pixels in the x direction
+    ynpix = number of pixels in the x direction
+
+    Example:
+    Detector(Point(0, 0, 0), Point(1, 0, 0), Point(0, 1, 0),
+             xnpix=100, ynpix=100)
+    """
     
     def __init__(self, p1, p2=None, p3=None, xnpix=1, ynpix=1):
         Plane.__init__(self, p1, p2, p3)
-        self.xextent = 2*(p2 - p1).length
-        self.yextent = 2*(p3 - p1).length
+        self.xwidth = 2*(p2 - p1).length
+        self.ywidth = 2*(p3 - p1).length
         self.xnpix = xnpix
         self.ynpix = ynpix
         self.pc = p1
@@ -19,25 +31,29 @@ class Detector(Plane):
         self.pixel_values = np.zeros((xnpix, ynpix))
 
     def intersection(self, o):
+        # Find intersections using base class method
         i = Plane.intersection(self, o)
 
-        # TODO: Find which pixel to increment
-        if i != []:
+        if len(i) == 1:
             i0 = (i[0] - self.pc)
             x0 = (self.px - self.pc).normalize()
             y0 = (self.py - self.pc).normalize()
             xlen = i0.dot(x0)
             ylen = i0.dot(y0)
 
-            # TODO: Clarify xextent (half or full width)
-            if abs(xlen) > self.xextent/2 or abs(ylen) > self.yextent/2:
-                pass
+            # Which pixel should increment?
+            if abs(xlen) > self.xwidth/2 or abs(ylen) > self.ywidth/2:
+                pass  # None; ray missed the detector
             else:
-                xfrac = xlen/self.xextent
-                yfrac = ylen/self.yextent
-                xind = np.round((self.xnpix - 1)*xfrac + (self.xnpix/2))
-                yind = np.round((self.ynpix - 1)*yfrac + (self.ynpix/2))
+                xfrac = xlen/self.xwidth
+                yfrac = ylen/self.ywidth
+                xind = np.round((self.xnpix - 1)*(0.5 + xfrac))
+                yind = np.round((self.ynpix - 1)*(0.5 + yfrac))
                 self.pixel_values[int(xind), int(yind)] += 1
-            
+        elif len(i) == 2:
+            assert("Warning: Ray intersects the detector twice!")
+        else:
+            pass
+        
         return i
         
--- pmst/tests/test_detector.py
+++ pmst/tests/test_detector.py
@@ -12,3 +12,14 @@ class TestDetector(TestCase):
         p3 = Point(1, 0, 2)
         d = Detector(p1, p2, p3, xnpix=100, ynpix=100)
         self.assertTrue(np.size(d.pixel_values) == 100*100)
+
+    def test_intersections(self):
+        p1 = Point(0, 0, 2)
+        p2 = Point(0, 1, 2)
+        p3 = Point(1, 0, 2)
+        d = Detector(p1, p2, p3, xnpix=2, ynpix=2)
+        r = Ray(Point(-0.1, -0.1, 0), Point (-0.1, -0.1, 1))
+        d.intersection(r)
+        r2 = Ray(Point(1, 1, 0), Point (1, 1, 1))        
+        d.intersection(r2)
+        np.testing.assert_array_equal(d.pixel_values, np.array(((1, 0), (0, 1))))
